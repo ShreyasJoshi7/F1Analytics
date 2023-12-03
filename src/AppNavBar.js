27940753a954620a414f02da78c9ffd613582496
Shreyas Joshi <shreyasjoshi.dev@gmail.com>
2023-12-02 20:20:45 -0800

    test

--- src/AppNavBar.js
+++ src/AppNavBar.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import {Link} from 'react-router-dom';
 
-// ""asdf" 
+// ""asdf" from local laptop
 
 export default class AppNavBar extends Component {
 
