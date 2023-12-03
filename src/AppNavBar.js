dde2a240446480e57c27b05debdebc7c19bbdefc
Shreyas Joshi <30567439+ShreyasJoshi7@users.noreply.github.com>
2023-12-02 19:37:28 -0800

    Update AppNavBar.js

--- src/AppNavBar.js
+++ src/AppNavBar.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import {Link} from 'react-router-dom';
 
-// ""asdf" 
+// ""asdf" from UI 
 
 export default class AppNavBar extends Component {
 
