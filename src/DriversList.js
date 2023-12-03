64c790828347adaa3a716dffc5ad5aec78767261
Shreyas Joshi <shreyasjoshi.dev@gmail.com>
2023-12-02 21:11:09 -0800

    git push to ci/cd to live

--- src/DriversList.js
+++ src/DriversList.js
@@ -197,7 +197,7 @@ render(){
     return(
       <div className="Table">
         <AppNavBar />
-        <Link to='/helloWorld'>ci/cd exists</Link>
+        <Link to='/helloWorld'>yeah</Link>
           <header className="Table-header">
             <div className="Table-intro">
                 <h2>Drivers</h2>
